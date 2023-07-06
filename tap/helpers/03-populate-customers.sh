1acc4c9a514cb858df9f92f098a9132a980e72d0
Olivier Mocquais <omocquais@vmware.com>
2023-07-06 14:33:05 +0200

    enable values for Spring Boot Actuator env endpoint

--- tap/helpers/03-populate-customers.sh
+++ tap/helpers/03-populate-customers.sh
@@ -5,11 +5,14 @@ url=$(kubectl get route -o yaml | yq '.items[0].status.url')
 echo "URL: $url"
 
 # Create a customer
+echo "Create a customer"
 uuid=$(curl -X POST "$url"/customer -H 'Content-Type: application/json' -d '{"firstName":  "John", "lastName": "Smith"}'  | jq -r .uuid)
 echo "uuid: $uuid"
 
 # Get the customer - 1st call (check in the database and populate the cache)
+echo "Get the customer - 1st call (check in the database and populate the cache)"
 curl -X GET $url/customer/$uuid | jq .
 
 # Get the customer - 2nd call (get data from the cache)
+echo "Get the customer - 2nd call (get data from the cache)"
 curl -X GET $url/customer/$uuid | jq .
