9408fb5410b764be87c30c3d5206cf9f73a4f83c
Olivier Mocquais <omocquais@vmware.com>
2023-10-27 09:51:24 +0200

    add new api.company.enabled property to call the external API

--- src/main/java/com/example/demospringboot/service/CompanyService.java
+++ src/main/java/com/example/demospringboot/service/CompanyService.java
@@ -2,15 +2,20 @@ package com.example.demospringboot.service;
 
 import com.example.demospringboot.dto.CustomerDTO;
 import io.micrometer.observation.annotation.Observed;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 public class CompanyService {
 
     private final CompanyApiClient companyApiClient;
+    private final boolean useApi;
 
-    public CompanyService(CompanyApiClient companyApiClient) {
+    public CompanyService(CompanyApiClient companyApiClient, @Value("${api.company.enabled:false}") boolean useApi) {
         this.companyApiClient = companyApiClient;
+        this.useApi = useApi;
     }
 
     @Observed(name = "getCompany",
@@ -18,6 +23,9 @@ public class CompanyService {
             lowCardinalityKeyValues = {"userType", "userType3"})
     public Company getCompany(CustomerDTO customer) {
         Integer id = (customer.firstName() + customer.lastName()).length();
-        return new Company(companyApiClient.getCompanyName(id).name());
+
+        String name = useApi ? companyApiClient.getCompanyName(id).name() : "Felucia-"+ Instant.now().toEpochMilli();
+
+        return new Company(name);
     }
 }
